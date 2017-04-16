04d3b4fe6eb6ee7898adc07e2d055cf8f0713cab
keertan dakarapu <keertandakarapu@gmail.com>
2017-04-15 19:35:04 -0500

    Creating a new class for vehicles to add different widths, heights for objects.

--- Roadrage_new/app/src/main/java/com/example/kd/roadrage_new/Vehicle_properties.java
+++ Roadrage_new/app/src/main/java/com/example/kd/roadrage_new/Vehicle_properties.java
@@ -5,4 +5,58 @@ package com.example.kd.roadrage_new;
  */
 
 public class Vehicle_properties {
+
+    public int width;
+    public int height;
+
+    public Vehicle_properties(int vehicle_type)
+    {
+        switch (vehicle_type)
+        {
+
+            case 0: //car 1
+                this.width = 150;
+                this.height = 284;
+                break;
+            case 1: //car 2
+                this.width = 150;
+                this.height = 284;
+                break;
+            case 2: //car 2
+                this.width = 150;
+                this.height = 284;
+                break;
+            case 3: //car 2
+                this.width = 150;
+                this.height = 284;
+                break;
+            case 4: //car 2
+                this.width = 150;
+                this.height = 284;
+                break;
+            case 5: //car 2
+                this.width = 150;
+                this.height = 284;
+                break;
+            case 11: //truck1
+                this.width = 200;
+                this.height = 500;
+                break;
+            case 12: //truck2
+                this.width = 200;
+                this.height = 500;
+                break;
+            case 13: //police car
+                this.width = 200;
+                this.height = 390;
+                break;
+            default: //truck1
+                this.width = 200;
+                this.height = 500;
+                break;
+
+        }
+
+    }
+
 }
